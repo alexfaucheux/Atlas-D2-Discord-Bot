310cc879bd3617625626ab6465e3b5407deac38d
alexfaucheux <afaucheux99@gmail.com>
2023-05-14 20:02:56 -0500

    fix query param check

--- utilities/endpointGenerator.js
+++ utilities/endpointGenerator.js
@@ -30,7 +30,7 @@ function generateEndpointString(endpoint) {
             paramObj.value = paramObj.default;
         }
 
-        if (paramObj.value === null && paramObj.requireda) {
+        if (paramObj.value === null && paramObj.required) {
             throw new Error('Query parameter object needs a value')
         }
 
