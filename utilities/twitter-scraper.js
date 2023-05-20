f3841f3f8f9bfe6c70accd8b6fa122dacb40b021
alexfaucheux <afaucheux99@gmail.com>
2023-05-19 21:18:25 -0500

    start of twitter scrape refactor

--- utilities/twitter-scraper.js
+++ utilities/twitter-scraper.js
@@ -1,11 +1,15 @@
 const execSync = require('child_process').execSync;
 const path = require('path');
 const fs = require('fs');
+const { default: axios } = require('axios');
+const cheerio = require('cheerio');
+const { hyperlink } = require('discord.js');
 
 const parentDir = path.dirname(__dirname);
 const folderPath = path.join(parentDir, 'twint-zero');
 const INSTANCE = 'nitter.1d4.us';
 const FORMAT = 'json';
+const twitterRootURI = 'https://www.twitter.com';
 
 const scrape = (query) =>
     execSync(
@@ -62,4 +66,44 @@ async function generateTweetFiles() {
 
         fs.writeFileSync(tweetFilePath, tweet);
     }
+}
+
+async function makeRequest(query) {
+    const tweets = [];
+    const resp = await axios.get(`https://${INSTANCE}/search?f=tweet&q=${query}`);
+    const $ = cheerio.load(resp.data);
+    let $tweet = $('div.timeline-item:first');
+
+    while ($tweet.length > 0) {
+        const tweetObj = {};
+        const $body = $tweet.find('div.tweet-body');
+        const $content = $body.find('div.tweet-content.media-body');
+        const $attachment = $body.find('div.attachments');
+        const textBody = populateHyperlinks($content);
+
+        tweetObj.body = textBody;
+        tweets.push(tweetObj);
+        $tweet = $tweet.next();
+    }
+    console.log(tweets);
+}
+
+function populateHyperlinks($content) {
+    let $anchor = $content.find('a:first');
+    let text = $content.text();
+
+    while ($anchor.length > 0) {
+        const uri = $anchor.attr('href');
+        const url = uri.includes('http') ? uri : twitterRootURI + $anchor.attr('href');
+        const $anchorText = $anchor.prop('innerText');
+        const textLink = hyperlink($anchorText, url);
+        text = text.replace($anchorText, textLink);
+        $anchor = $anchor.next();
+    }
+
+    return text;
+}
+
+if (require.main === module) {
+    makeRequest('from:primegaming');
 }
