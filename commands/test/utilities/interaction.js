b2ffda7f0d7f76a5f7faa687bbae8caa02c59b43
alexfaucheux <afaucheux99@gmail.com>
2023-05-20 19:39:46 -0500

    Update test command handling

--- commands/test/utilities/interaction.js
+++ commands/test/utilities/interaction.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('inter')
-		.setDescription('Provides information about the interaction.'),
+		.setDescription('(TEST COMMAND) Provides information about the interaction.'),
 	async execute(interaction) {
 		console.log(interaction);
 		// interaction.guild is the object representing the Guild in which the command was run
